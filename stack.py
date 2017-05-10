6cbe2a86e46c43475908256bef71fc251abb18fb
Nathan Suh <nathansuh1@gmail.com>
2017-05-10 15:56:47 -0700

    update stack

--- stack.py
+++ stack.py
@@ -13,13 +13,26 @@ class Stack:
     def isEmpty(self):
         if len(self.elements) == 0:
             return True
-        else
+        else:
             return False
 
     def push(self, value):
         self.elements.insert(0, value) #inserts in front of 0th position
 
     def pop(self):
-        self.elements.pop() #lol
+        return self.elements.pop(0) 
 
+    def top(self):
+        return self.elements[0]
 
+if __name__ == "__main__":
+    myStack = Stack()
+    myStack.push(10)
+    myStack.push(20)
+    myStack.push(30)
+    print myStack.top()
+    myStack.pop()
+    print myStack.top()
+    myStack.pop()
+    print myStack.top()
+    myStack.pop()
